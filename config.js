b00ce3b4b90f05b26ea23d19e13ec45ac933dde8
thinker33 <92138643+thinker33@users.noreply.github.com>
2022-06-30 13:11:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ const chalk = require("chalk");
 // Other
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "917003213983";
+  gg = "916307568766";
 }
 
 global.owner = gg.split(",");
 global.mongodb = process.env.MONGODB || "none";
 global.pgdb = process.env.DATABASE_URL || "none";
 global.port = process.env.PORT || 8080;
-(global.packname = process.env.PACKNAME || "kaguya"),
+(global.packname = process.env.PACKNAME || "thinker"),
   (global.prefix = process.env.PREFIX || "-"),
   (global.author = process.env.AUTHOR || "ARUS"),
   (global.sessionName = "arus"),
